bc1bc5ba03d46c2c18b019090c46d643db38a0e4
Silla Pranav Kumar <pranavsilla@gmail.com>
2025-12-25 12:00:42 +0530

    updated next.config

--- voterapp/next.config.mjs
+++ voterapp/next.config.mjs
@@ -1,15 +1,24 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/api/:path*", // The path you want to use in your frontend
+//         destination: "http://localhost:8000/api/:path*", // The external API destination
+//       },
+//     ];
+//   },
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*", // The path you want to use in your frontend
-        destination: "http://localhost:8000/api/:path*", // The external API destination
-      },
-    ];
-  },
   /* config options here */
   reactCompiler: true,
+  // We removed the async rewrites() function entirely
 };
 
 export default nextConfig;
